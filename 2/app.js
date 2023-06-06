73e6a6e2bcbb6ce43a51ca35eda80ddf9f2afa6a
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-06-06 23:53:19 +0530

    Express - handling HTTP requests

--- 2/app.js
+++ 2/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const fs = require('fs');
 
 let app = express();
+app.use(express.json());
+
 let movies = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
 
-//GET - api/movies
+//GET - api/movies(version1)
 app.get("/api/v1/movies", (req, res)=>{
     res.status(200).json({
         status:"success",
@@ -13,6 +15,23 @@ app.get("/api/v1/movies", (req, res)=>{
             movies:movies
         }
     });
+})
+
+//POST - api/movies(version1)
+app.post("/api/v1/movies", (req, res)=>{
+    // console.log(req.body);
+    const newId = movies[movies.length-1].id + 1;
+    let newMovie = Object.assign({id:newId}, req.body);
+    movies.push(newMovie);
+    fs.writeFile("./movies.json", JSON.stringify(movies), (err)=>{
+        res.status(201).json({
+            status:"success",
+            data:{
+                movie:newMovie
+            }
+        })
+    });
+    // res.send("Created");
 })
 
 const PORT = 3000;
