ddc483818c9ebcbcff060d53e28e21c1d7a1af8b
Siddharth Garg <siddharthgarg16082000@gmail.com>
2023-06-09 01:47:51 +0530

    Express - handling HTTP requests

--- 2/app.js
+++ 2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 let app = express();
-app.use(express.json());
+app.use(express.json()); //middleware
 
 let movies = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
 
